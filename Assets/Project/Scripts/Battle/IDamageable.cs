cf2ba617efbe358479d19a3d3a990643aa7a8841
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2023-03-03 22:44:23 +0300

    add: start development battle

--- Assets/Project/Scripts/Battle/IDamageable.cs
+++ Assets/Project/Scripts/Battle/IDamageable.cs
@@ -1,7 +1,22 @@
-﻿namespace SlimeRPG.Battle
+﻿using SlimeRPG.Data;
+using UnityEngine;
+
+namespace SlimeRPG.Battle
 {
     public interface IDamageable<T>
     {
         void Damage(T damage);
+    }
+
+    public class Shooter : MonoBehaviour
+    {
+        [SerializeField] private Projectile _projectile;
+        [SerializeField] private InitialAbilityValue _atk;
+        [SerializeField] private InitialAbilityValue _aspd;
+    }
+
+    public class Projectile : MonoBehaviour
+    {
+
     }
 }
