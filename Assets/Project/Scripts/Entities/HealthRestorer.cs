47db262591f75efe4dee108f85a4e529b4e8fe21
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2023-03-04 01:16:59 +0300

    add: health restorer

---
+++ Assets/Project/Scripts/Entities/HealthRestorer.cs
@@ -0,0 +1,30 @@
+﻿using SlimeRPG.Data;
+using UnityEngine;
+
+namespace SlimeRPG.Entities
+{
+    public class HealthRestorer : MonoBehaviour
+    {
+        [SerializeField] private Health _health;
+        [SerializeField] private InitialAbilityValue _healthRecovery;
+        [SerializeField][Min(0)] private float _delay;
+
+        private float _expandedTime = 0;
+        private float _current;
+
+        private void Awake()
+        {
+            _current = _healthRecovery.Value;
+        }
+
+        private void Update()
+        {
+            _expandedTime += Time.deltaTime;
+            if (_expandedTime < _delay)
+                return;
+
+            _health.Add(_current);
+            _expandedTime = 0;
+        }
+    }
+}
