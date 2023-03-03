ecb37a92e9f5b7f6688649071d39f1420ec55425
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2023-03-04 01:04:02 +0300

    rollback

---
+++ Assets/Project/Scripts/Entities/PlayerMovementIllusion.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace SlimeRPG.Entities
+{
+    public class PlayerMovementIllusion : MonoBehaviour
+    {
+        [SerializeField] private EnemyWave _wave;
+        [SerializeField] private Vector3 _offset;
+
+        private Transform _camera;
+
+        private void Awake()
+        {
+            _camera = transform;
+        }
+
+        private void OnEnable()
+        {
+            _wave.OnWaveStarted += OnMove;
+        }
+        private void OnDisable()
+        {
+            _wave.OnWaveStarted -= OnMove;
+        }
+
+        private void OnMove()
+        {
+
+        }
+    }
+}
